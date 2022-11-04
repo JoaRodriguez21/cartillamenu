8a3cf151617407a813b247305a669646676ee3ac
JoaRodriguez21 <joaquinrodriguez0012@gmail.com>
2022-11-04 20:16:44 -0300

    Se añadieron objetos

--- script.js
+++ script.js
@@ -1,90 +1,122 @@
-//Sistema de login del sitio
-    //Le pido los datos al usuario
+// Simulador Menú restorant
 
-//funciones
+let totalCompra = 0
+let seguirComprando = true
+let decision
 
-function calculoEdad(p2){
-    let resultado = 2022 - p2
-    return resultado
-};
+class Producto{
+  constructor(id, nombre, categoria, precio){
+  this.id = id
+  this.nombre = nombre;
+  this.categoria = categoria;
+  this.precio = precio;
+  }
 
-//Condicionales (se le pide el año al usuario para calcular su edad)
-alert("Registrate para entrar en la plataforma.");
-const año = parseInt(prompt("Ingrese su año de nacimiento"));
-let edad = calculoEdad(año);
-console.log(edad)
-
-while(edad<18 || edad>100){
-    if(edad>=18 && edad<=100){
-        break
-    } else if(edad>100 || edad<=0){
-        alert("Los datos ingresados son invalidos")
-    } else {
-        alert("Necesitas tener más de 18 años para ingresar a la plataforma")
-    }
 }
 
-alert("Edad valida! Ahora ingresá un usuario y una contraseña")
+const hamburguesaPollo = new Producto(1,"Hamburguesa de Pollo","Hamburguesas",1200)
+const hamburguesaCarne = new Producto(2, "Hamburguesa de Carne", "Hamburguesas", 1400)
+const hamburguesaVegie = new Producto(3, "Hamburguesa Vegetariana", "Hamburguesas", 900)
+const hamburguesaSoja = new Producto(4, "Hamburguesa de Soja", "Hamburguesas", 1100)
+const papasGrandes = new Producto(5, "Papas fritas Grandes", "Papas fritas", 800)
+const papasChicas = new Producto(6, "Papas fritas Chicas", "Papas fritas", 500)
+const cocacola = new Producto(7, "Vaso de Coca-Cola", "Bebida", 300)
+const sprite = new Producto(8, "Vaso de Sprite", "Bebida", 300)
 
-//declaro la variables de usuarios
 
-let  usuario = prompt("Ingrese su nombre de usuario");
-console.log(usuario);
-let usuario2 = prompt("Vuelva a ingresar el nombre de usuario");
-console.log(usuario2);
-    
-while(usuario != usuario2){
-    alert("El nombre de usuario no es el mismo al ingresado, intentelo de nuevo.");
-    let  usuario = prompt("Ingrese su nombre de usuario");
-    console.log(usuario);
-    let usuario2 = prompt("Vuelva a ingresar el nombre de usuario");
-    console.log(usuario2);
-    if(usuario === usuario2){
-        break
-    } else {
-        continue
-    }
+while (seguirComprando = true){
+  let producto = parseInt(prompt(`Escoge el plato principal de su pedido: 
+  1.${hamburguesaPollo.nombre} ($${hamburguesaPollo.precio})
+  2.${hamburguesaCarne.nombre} ($${hamburguesaCarne.precio})
+  3.${hamburguesaVegie.nombre} ($${hamburguesaVegie.precio}) 
+  4.${hamburguesaSoja.nombre} ($${hamburguesaSoja.precio})`))
+  
+  if (producto === 1){
+    //hamburguesa pollo
+    totalCompra = totalCompra + hamburguesaPollo.precio
+  } else if (producto === 2) {
+    //hamburguesa carne
+    totalCompra = totalCompra + hamburguesaCarne.precio
+  } else if (producto === 3) {
+    // hamburguesa vegetariana
+    totalCompra = totalCompra + hamburguesaVegie.precio
+  } else if (producto === 4) {
+    //hamburguesa Soja
+    totalCompra + totalCompra + hamburguesaSoja.precio
+  } else {
+    alert(`Error al escoger plato principal, vuelve a intentarlo.`)
+    continue
+  }
+
+  decision = parseInt(prompt(`Si desea agregar otro plato principal presione "1".
+  Si desea continuar presione cualquier tecla`))
+  if(decision === 1) {
+    continue
+  } else{
+   break
+  }
+
 }
+console.log(totalCompra)
+seguirComprando = true
 
-alert("Ahora ingrese una contraseña:");
+while (seguirComprando = true){
+  let producto2 = parseInt(prompt(`Escoge un acompañamiento para su pedido: 
+  1.${papasChicas.nombre}($${papasChicas.precio}) 
+  2.${papasGrandes.nombre}($${papasGrandes.precio}) 
+  3.Sin Acompañamiento`))
+  if (producto2 === 1){
+    //papas chicas
+    totalCompra = totalCompra + papasChicas.precio
+  } else if (producto2 === 2){
+    //papas grandes
+    totalCompra = totalCompra + papasGrandes.precio
+  } else if (producto2 === 3){
+    //sin acompañamiento
+    break
+  } else {
+    alert(`La opción seleccionada no esta disponible, vuelve a intentarlo nuevamente`)
+    continue
+  }
 
-//declaro la variables contraseñas
+  decision = parseInt(prompt(`Si desea agregar otro acompañamiento presione "1".
+  Si desea continuar precione cualquier tecla`))
+  if(decision === 1) {
+    continue
+  } else {
+    break
+  }
+} 
 
-let contraseña = prompt("Contraseña");
-console.log(contraseña);
-let contraseña2 = prompt("Vuelve a ingresar la contraseña");
-console.log(contraseña2)
+console.log(totalCompra)
+seguirComprando = true
 
-while(contraseña != contraseña2){
-    alert("la contraseña no es la misma a la ingresada, intentelo de nuevo")
-    let contraseña = prompt("Contraseña");
-    console.log(contraseña);
-    let contraseña2 = prompt("Vuelve a ingresar la contraseña");
-    console.log(contraseña2)
-    if(contraseña === contraseña2){
-        break
-    } else {
-        continue
-    }
+while(seguirComprando = true){
+  let producto3 = parseInt(prompt(`Escoge una bebida para su pedido 
+  1.${cocacola.nombre}($${cocacola.precio}) 
+  2.${sprite.nombre}($${sprite.precio}) 
+  3.Sin Bebidas`))
+  if (producto3 === 1){
+    //bebida cocacola
+    totalCompra = totalCompra + cocacola.precio
+  } else if (producto3 === 2){
+    //bebida sprite
+    totalCompra = totalCompra + sprite.precio
+  } else if (producto3 === 3){
+    //sin bebida
+    break
+  } else {
+    alert(`Escoge una bebida VALIDa para su pedido`)
+    continue
+  }
+
+  decision = parseInt(prompt(`Si desea agregar otra bebida presione "1".
+  Si desea continuar precione cualquier tecla`))
+  if(decision === 1) {
+    continue
+  } else {
+    break
+  }
 }
-
-alert("Gracias por registrarte! ahora inicia sesión para ingresar a la plataforma");
-
-let usuarioInicio
-let contraseñaInicio
-
-//CICLOS
-
-while(usuario2 != usuarioInicio || contraseña2 != contraseñaInicio){
-    let usuarioInicio = prompt("Ingrese el nombre de usuario:");
-    let contraseñaInicio = prompt("Ingresa la contraseña");
-    if(usuarioInicio != usuario || contraseñaInicio != contraseña){
-        alert(`Usuario o contraseña incorrecto, vuelva a intentarlo`)
-        console.log("el usuario no ingresa a la plataforma") 
-        continue
-    } else {
-        break
-    }
-}
-alert(`Inicio de sesión correcto, bienvenido ${usuario}`);
-console.log("el usuario ingresa a la plataforma")
+console.log(totalCompra)
+alert (`El total de tu compra es ${totalCompra}`)
