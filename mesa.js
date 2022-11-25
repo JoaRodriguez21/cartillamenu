46e08c67598a18bc7c6f4fdb7d9657a928caf019
JoaRodriguez21 <joaquinrodriguez0012@gmail.com>
2022-11-24 21:50:26 -0300

    carrito display none & block

--- mesa.js
+++ mesa.js
@@ -1,13 +1,14 @@
 //info de los input
-const inputCliente =  document.getElementById("inputCliente")
-const inputMesa = document.getElementById("inputMesa")
-const botonIngresar =  document.getElementById("botonIngresar")
+const inputCliente =  document.getElementById("inputCliente");
+const inputMesa = document.getElementById("inputMesa");
+const botonIngresar =  document.getElementById("botonIngresar");
 //tomo los strings del HTML
-const titulo = document.getElementById("titulo")
-const divTitulo = document.getElementById("divTitulo")
-const divMesa = document.getElementById("divMesa")
-const imgCarrito = document.getElementById("imgCarrito")
-const divProductos = document.getElementById("divProductos")
+const titulo = document.getElementById("titulo");
+const divTitulo = document.getElementById("divTitulo");
+const divMesa = document.getElementById("divMesa");
+const imgCarrito = document.getElementById("imgCarrito");
+const divProductos = document.getElementById("divProductos");
+const divCarrito = document.getElementById("divCarrito");
 
 const cliente = 
 //guardo info en el storage
@@ -25,25 +26,14 @@ botonIngresar.onclick = () => {
         titulo.innerText = `Bienvenido a Cofe Shop, ya puedes realizar tu pedido`
         divTitulo.append(tituloMesa)
         divMesa.remove()
-        imgCarrito.innerHTML = `<div class="contImgCarrito">
-        <img src="img/iconos/carrito.svg" alt="" type="button" class="logoCarrito" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-      </div>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-        <div class="offcanvas-header">
-          <h3 class="offcanvas-title" id="offcanvasRightLabel">Carrito</h3>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body" id="divCarrito">
-          ...
-        </div>
-      </div>`
+        divCarrito.className = "displayCarritoB"
     }
     
 }
 
 //tomo la info del storage
-const nombreCliente = localStorage.getItem("usuario")
-const numeroMesa = localStorage.getItem("mesa")
+const nombreCliente = localStorage.getItem("usuario");
+const numeroMesa = localStorage.getItem("mesa");
 
 //creo el elemento que se va a mostrar una vez el usuario ingrese
 
--- shop.js
+++ shop.js
@@ -1,18 +1,18 @@
 //DIV PADRES
 const shopBebidasCalientes = document.getElementById("shopBebidasCalientes")
 
+
 //Array carrito: base de datos del carrito
 carrito = []
  
 bebidasCalientes.forEach((arrayBebidasCalientes) => {
   let divBebidasCalientes = document.createElement("div");
   divBebidasCalientes.classList.add("classProductos")
+    divBebidasCalientes.className = "card-body cardsProductos"
     divBebidasCalientes.innerHTML +=`
-    <div class="card-body" style="width: 18rem;">
       <h3 class="card-title">${arrayBebidasCalientes.nombre}</h3>
       <p class="card-text">${arrayBebidasCalientes.categoria}</p>
       <p class="card-text">$${arrayBebidasCalientes.precio}</p>
-    </div>
     `;
     shopBebidasCalientes.append(divBebidasCalientes)
     //Crear boton comprar
